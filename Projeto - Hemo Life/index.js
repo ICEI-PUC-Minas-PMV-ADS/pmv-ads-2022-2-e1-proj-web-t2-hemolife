fd21a679a885ac5f3bc74a5e8bd6851fad2822a2
Lucas Reis <113260706+ReisLucasF@users.noreply.github.com>
2022-10-20 00:43:20 -0300

    Update index.js

--- Projeto - Hemo Life/index.js
+++ Projeto - Hemo Life/index.js
@@ -28,3 +28,20 @@ const navSlide = () => {
 }   
 
 navSlide();
+
+//LOgin 
+
+function logar(){
+
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "admin" && senha == "admin"){
+        alert('Login Efetuado! você será redirecionado.');
+    }
+
+    else{
+        alert('Usuário ou senha incorretos');
+    }
+
+    }
