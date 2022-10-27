180de16308e38254b26dfdb1323c27e031165479
Gabriel Oliveira <110210793+Gabriel-Alisson@users.noreply.github.com>
2022-10-27 18:32:24 -0300

    Add files via upload

---
+++ bloodlevel.js
@@ -0,0 +1,120 @@
+window.addEventListener('load', function() {
+
+    var rangeslider = document.getElementById("sliderRange");
+  
+    var images = document.getElementById("sliderImages");
+  
+    rangeslider.addEventListener('input', function() {
+      for (var i = 0; i < images.children.length; i++) {
+        images.children[i].style.display = 'none';
+      }
+      i = Number(this.value) - 1;
+      images.children[i].style.display = 'block';
+    });
+  
+  });
+  window.addEventListener('load', function() {
+  
+    var rangeslider = document.getElementById("sliderRange2");
+  
+    var images = document.getElementById("sliderImages2");
+  
+    rangeslider.addEventListener('input', function() {
+      for (var i = 0; i < images.children.length; i++) {
+        images.children[i].style.display = 'none';
+      }
+      i = Number(this.value) - 1;
+      images.children[i].style.display = 'block';
+    });
+  
+  });
+  window.addEventListener('load', function() {
+  
+    var rangeslider = document.getElementById("sliderRange3");
+  
+    var images = document.getElementById("sliderImages3");
+  
+    rangeslider.addEventListener('input', function() {
+      for (var i = 0; i < images.children.length; i++) {
+        images.children[i].style.display = 'none';
+      }
+      i = Number(this.value) - 1;
+      images.children[i].style.display = 'block';
+    });
+  
+  });
+  window.addEventListener('load', function() {
+  
+    var rangeslider = document.getElementById("sliderRange4");
+  
+    var images = document.getElementById("sliderImages4");
+  
+    rangeslider.addEventListener('input', function() {
+      for (var i = 0; i < images.children.length; i++) {
+        images.children[i].style.display = 'none';
+      }
+      i = Number(this.value) - 1;
+      images.children[i].style.display = 'block';
+    });
+  
+  });
+  window.addEventListener('load', function() {
+  
+    var rangeslider = document.getElementById("sliderRange5");
+  
+    var images = document.getElementById("sliderImages5");
+  
+    rangeslider.addEventListener('input', function() {
+      for (var i = 0; i < images.children.length; i++) {
+        images.children[i].style.display = 'none';
+      }
+      i = Number(this.value) - 1;
+      images.children[i].style.display = 'block';
+    });
+  
+  });
+  window.addEventListener('load', function() {
+  
+    var rangeslider = document.getElementById("sliderRange6");
+  
+    var images = document.getElementById("sliderImages6");
+  
+    rangeslider.addEventListener('input', function() {
+      for (var i = 0; i < images.children.length; i++) {
+        images.children[i].style.display = 'none';
+      }
+      i = Number(this.value) - 1;
+      images.children[i].style.display = 'block';
+    });
+  
+  });
+  window.addEventListener('load', function() {
+  
+    var rangeslider = document.getElementById("sliderRange7");
+  
+    var images = document.getElementById("sliderImages7");
+  
+    rangeslider.addEventListener('input', function() {
+      for (var i = 0; i < images.children.length; i++) {
+        images.children[i].style.display = 'none';
+      }
+      i = Number(this.value) - 1;
+      images.children[i].style.display = 'block';
+    });
+  
+  });
+  window.addEventListener('load', function() {
+  
+    var rangeslider = document.getElementById("sliderRange8");
+  
+    var images = document.getElementById("sliderImages8");
+  
+    rangeslider.addEventListener('input', function() {
+      for (var i = 0; i < images.children.length; i++) {
+        images.children[i].style.display = 'none';
+      }
+      i = Number(this.value) - 1;
+      images.children[i].style.display = 'block';
+    });
+  
+  });
