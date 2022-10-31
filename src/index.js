1ed224c0c1ae63b929f79a65c7e2587bbd6cac4a
Lucas Reis <113260706+ReisLucasF@users.noreply.github.com>
2022-10-31 11:54:41 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -37,13 +37,11 @@ function logar(){
     var senha = document.getElementById('senha').value;
 
     if(login == "admin" && senha == "admin"){
-        alert('Login Efetuado! você será redirecionado.');{
             location.href = "acesso-autorizado.html"
         };
     }
 
     else{
-        alert('Usuário ou senha incorretos');{
             location.href = "acesso-negado.html"
         };
     }
