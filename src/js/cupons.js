6d3cede45410f7e59a3e715b6767596918d73f62
JansemWall <114194318+JansemWall@users.noreply.github.com>
2022-11-26 22:16:45 +0000

    Novo estilo cartão e js para cupons

---
+++ src/js/cupons.js
@@ -0,0 +1,58 @@
+var db = {
+    dados: [
+        {
+            id: "farmacia",
+            src: "../img/farmacia.png",
+            beneficio: "30% de desconto",
+            cupom: "30DESCONTO",
+        },
+        {
+            id: "farmacia",
+            src: "../img/farmacia.png",
+            beneficio: "1 ano frete grátis",
+            cupom: "FRETEGRATIS",
+        },
+        {
+            id: "farmacia",
+            src: "../img/farmacia.png",
+            beneficio: "15% Dermocosméticos",
+            cupom: "15DERMO"
+        },
+        {
+            id: "academia",
+            src: "../img/academia.png",
+            beneficio: "20% de desconto",
+            cupom: "20DESCONTO"
+        },
+        {
+            id: "academia",
+            src: "../img/academia.png",
+            beneficio: "Matrícula grátis",
+            cupom: "MATRICULAFREE"
+        },
+        {
+            id: "academia",
+            src: "../img/academia.png",
+            beneficio: "Kit Whey e Creatina",
+            cupom: "KITWHEYCREATINA",
+        },
+        {
+            id: "mercado",
+            src: "../img/mercado.png",
+            beneficio: "15% de desconto",
+            cupom: "15DESCONTO",
+        },
+        {
+            id: "mercado",
+            src: "../img/mercado.png",
+            beneficio: "30% desconto Hortifruti",
+            cupom: "FRETEGRATIS",
+        },
+        {
+            id: "mercado",
+            src: "../img/mercado.png",
+            beneficio: "Vale 300 reais",
+            cupom: "VALECOMPRAS300",
+        }
+    ]
+}
