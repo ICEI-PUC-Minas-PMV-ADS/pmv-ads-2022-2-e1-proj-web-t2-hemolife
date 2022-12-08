03538e33e23bef25e706f89df448a77e4023f5e4
JansemWall <114194318+JansemWall@users.noreply.github.com>
2022-12-08 14:02:50 -0300

    Update criar-conta.js

--- src/js/criar-conta.js
+++ src/js/criar-conta.js
@@ -143,7 +143,7 @@ function cadastroCom() {
         localStorage.setItem('usurLista', JSON.stringify(usurLista))
         setTimeout(() => {
             /*Cadastro Efetuado*/
-            window.location.pathname = "./src/pages/login.html";
+            window.location.pathname = "././src/pages/login.html";
         }, 2000);
         msgLog.innerHTML = ''
     }
